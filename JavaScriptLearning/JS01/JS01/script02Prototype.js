07aa9e3755f3cf3da2f2d3375ad09484d21a5a8b
SylvesterLi <lisangyu45@qq.com>
2018-10-04 14:35:21 +0800

    prototype

---
+++ JavaScriptLearning/JS01/JS01/script02Prototype.js
@@ -0,0 +1,22 @@
+﻿function Person(name, age, address) {
+    this.user_name = name;
+    this.user_age = age;
+    this.user_address = address;
+}
+
+Person.prototype.sayHi = function () {
+    alert(this.user_name + '  ' + this.user_age + '   ' + this.user_address);
+};
+
+var v1 = new Person();
+v1.user_address = '地址1';
+v1.user_age = 10;
+v1.user_name = 'USR1';
+
+var v2 = new Person();
+v2.user_address = '地址2';
+v2.user_age = 12;
+v2.user_name = 'USR2';
+
+v1.sayHi();
+v2.sayHi();
