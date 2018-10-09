d2bed187673b207b5723d7cda4e0687d41ad7c56
老李拉面馆 <narisdrum@outlook.com>
2018-10-09 15:44:20 +0800

    更改标题

---
+++ JavaScriptLearning/JS01/JS01/ChangeTitle.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+    setInterval(function () {
+        var title = document.title;
+        var firstChar = title.charAt(0);
+        var othChar = title.substring(1);
+        document.title = othChar + firstChar;
+
+        //尾换头
+    }, 1000);
+
+    //需要有标题没有标题就没有效果
+
+    
+
+};
+
+window.onbeforeunload = function () {
+    //window.event.returnValue = false;
+    return '你真的要离开吗emmmmm....';
+};
