51de95a7e20e42e23b811e7ac19ce7ff9ab346d8
老李拉面馆 <narisdrum@outlook.com>
2018-10-09 17:23:14 +0800

    4 done

---
+++ JavaScriptLearning/JS01/JS01/WindowObject.js
@@ -0,0 +1,17 @@
+﻿window.onload = function () {
+
+    //document.getElementById('portNum').value = window.location.port;
+    //document.getElementById('hostName').value = window.location.hostname;
+
+    //for (var key in window.location) {
+    //    console.log(key + '  : ' + window.location[key]);
+    //}
+    //clipboardData.setData('text', '23231234324');
+    //console.log('Copied!');
+
+    //console.log(clipboardData.getData('text'));
+
+    //document.write('hellow!!');
+
+
+};
