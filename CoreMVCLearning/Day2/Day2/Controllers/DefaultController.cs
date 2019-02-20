970567ba1c2fb23901472f032cbaf2e194003cc9
老李拉面馆 <sangyu.li@outlook.com>
2019-02-20 19:37:32 +0800

    1 action result bug fixed

---
+++ CoreMVCLearning/Day2/Day2/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using Day2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            //return Name
+            return View();
+        }
+        public ActionResult Rea2(FormCollection m)
+        {
+            string name = m["name"];
+            string  id = m["id"];
+            return Content(name + "\t" + id);
+        }
+        
+    }
+}
