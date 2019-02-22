bc03eac928a1f146b6617eb6ea90e4f4af99b0d7
老李拉面馆 <sangyu.li@outlook.com>
2019-02-22 12:57:25 +0800

    Update DefaultController.cs

--- CoreMVCLearning/Day2/Day2/Controllers/DefaultController.cs
+++ CoreMVCLearning/Day2/Day2/Controllers/DefaultController.cs
@@ -28,6 +28,9 @@ namespace Day2.Controllers
         [HttpGet]
         public ActionResult Edit()
         {
+            //在这读取数据库内容
+            //并return View(Your Model);
+            //在Get方法里展示数据库当前的内容
             IndexModel indexModel = new IndexModel();
             indexModel.Id = 12;
             indexModel.Name = "234safg";
@@ -37,6 +40,10 @@ namespace Day2.Controllers
         [HttpPost]
         public ActionResult Edit(FormCollection m, HttpPostedFileBase fileBase)
         {
+
+            //点击Submit后
+            //在Post方法里进行数据库的Update操作
+            //这里需要读取当前View展示的东西【数据从Edit(这里传进来)】
             fileBase.SaveAs(Server.MapPath("~/" + fileBase.FileName));
             string name = m["name"];
             string id = m["id"];
