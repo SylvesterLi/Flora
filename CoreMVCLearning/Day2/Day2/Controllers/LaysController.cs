10f997e50a43c6af955d8eab51b3899575ad6840
老李拉面馆 <sangyu.li@outlook.com>
2019-02-23 12:36:13 +0800

    add render body section

---
+++ CoreMVCLearning/Day2/Day2/Controllers/LaysController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2.Controllers
+{
+    public class LaysController : Controller
+    {
+        // GET: Lays
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult L1()
+        {
+            return View();
+        }
+    }
+}
